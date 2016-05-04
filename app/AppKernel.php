5b90522f542d676b8a551190b057752278966778
Jordi <jordi@jordiroig.com>
2016-05-04 23:47:52 +0000

    Code cleaning

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -15,8 +15,8 @@ class AppKernel extends Kernel
             new Symfony\Bundle\SwiftmailerBundle\SwiftmailerBundle(),
             new Doctrine\Bundle\DoctrineBundle\DoctrineBundle(),
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
-	    new FOS\RestBundle\FOSRestBundle(),
-	    new JMS\SerializerBundle\JMSSerializerBundle(),
+			new FOS\RestBundle\FOSRestBundle(),
+			new JMS\SerializerBundle\JMSSerializerBundle(),
             new Letgo\TwitterBundle\LetgoTwitterBundle(),
         );
 
--- src/Letgo/TwitterBundle/Controller/TwitterController.php
+++ src/Letgo/TwitterBundle/Controller/TwitterController.php
@@ -6,12 +6,9 @@ use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use FOS\RestBundle\Controller\FOSRestController;
 use FOS\RestBundle\Controller\Annotations\Get;
-use FOS\RestBundle\Controller\Annotations\Prefix;
 
 class TwitterController extends FOSRestController
 {
-	
-	
     /**
      * @Route("/")
      */
