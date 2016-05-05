f1dcd39e48dbc503db0e2b4a822b943990379551
Jordi <jordi@jordiroig.com>
2016-05-05 12:14:15 +0200

    First trying with Guzzle

--- src/Letgo/TwitterBundle/Controller/TwitterController.php
+++ src/Letgo/TwitterBundle/Controller/TwitterController.php
@@ -2,7 +2,6 @@
 
 namespace Letgo\TwitterBundle\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use FOS\RestBundle\Controller\FOSRestController;
 use FOS\RestBundle\Controller\Annotations\Get;
@@ -20,9 +19,9 @@ class TwitterController extends FOSRestController
     /**
 	 * @Get("/tweets/{name}", name="get_tweets")
 	 */   
-    public function getTweetsAction($name)
+    public function getTweetsAction($username)
     {
-	    $prova = $this->get('guzzle.twitter.client')->get('statuses/user_timeline.json?screen_name='.$name.'&count=5')->send()->json();
+	    $prova = $this->get('letgo.twitter.reader')->get('statuses/user_timeline.json?screen_name='.$username.'&count=5')->send()->json();
 	    die(var_dump($prova));
 	    return $name;
     }
--- src/Letgo/TwitterBundle/Services/Twitter/Reader.php
+++ src/Letgo/TwitterBundle/Services/Twitter/Reader.php
@@ -2,10 +2,29 @@
 
 namespace Letgo\TwitterBundle\Services\Twitter;
 
+use GuzzleHttp\Client;
+use GuzzleHttp\HandlerStack;
+use GuzzleHttp\Subscriber\Oauth\Oauth1;
+
 class Reader
 {
 	public function getTweets($username)
 	{
-		return "prova amb ".$username;	
+		$stack = HandlerStack::create();
+		$oauth = new Oauth1([
+			'consumer_key'    => 'my_key',
+			'consumer_secret' => 'my_secret',
+			'token'           => 'my_token',
+			'token_secret'    => 'my_token_secret'
+		]);
+		$stack->push($oauth);
+		$client = new Client([
+			'base_uri' => 'https://api.twitter.com',
+			'handler' => $stack,
+			'defaults' => ['auth' => 'oauth']
+		]);
+
+		$res = $client->get('statuses/home_timeline.json?screen_name='.$username.'&count=5');
+		return $res;
 	}
 }
