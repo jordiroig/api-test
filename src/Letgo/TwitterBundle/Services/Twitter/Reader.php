4ee5c55f4da3ea2525b729c1e0a3ce3910de83f1
Jordi <jordi@jordiroig.com>
2016-05-05 13:48:00 +0200

    Refactoring

--- src/Letgo/TwitterBundle/Services/Twitter/Reader.php
+++ src/Letgo/TwitterBundle/Services/Twitter/Reader.php
@@ -2,22 +2,34 @@
 
 namespace Letgo\TwitterBundle\Services\Twitter;
 
-use GuzzleHttp\Client as GuzzleHttpClient;
+use GuzzleHttp\Client;
+use GuzzleHttp\HandlerStack;
+use GuzzleHttp\Subscriber\Oauth\Oauth1;
 
 class Reader
 {
-	private $twitter_client;
+	private $oauth1;
+	private $base_url;
 
 	public function __construct(
-		GuzzleHttpClient $twitter_client
+		Oauth1 $oauth1,
+		$base_url
 	) {
-		$this->twitter_client = $twitter_client;
+		$this->oauth1 = $oauth1;
+		$this->base_url = $base_url;
 	}
 
 	public function getTweets($username)
 	{
-		$res = $this->twitter_client->get('statuses/home_timeline.json?screen_name='.$username.'&count=5');
+		$stack = HandlerStack::create();
+		$stack->push($this->oauth1);
+		$client = new Client([
+			'base_uri' => $this->base_url,
+			'handler' => $stack,
+			'auth' => 'oauth'
+		]);
 
+		$res = $client->get('statuses/home_timeline.json?screen_name='.$username.'&count=5')->getBody();
 		return $res;
 	}
 }
