29cddde13ee9398cf034bed199b16692efa562e8
Jordi <jordi@jordiroig.com>
2016-05-05 13:39:26 +0200

    Refactoring

--- src/Letgo/TwitterBundle/Services/Twitter/Reader.php
+++ src/Letgo/TwitterBundle/Services/Twitter/Reader.php
@@ -2,40 +2,22 @@
 
 namespace Letgo\TwitterBundle\Services\Twitter;
 
-use GuzzleHttp\Client;
-use GuzzleHttp\HandlerStack;
-use GuzzleHttp\Subscriber\Oauth\Oauth1;
+use GuzzleHttp\Client as GuzzleHttpClient;
 
 class Reader
 {
-	private $oauth1;
-	private $base_url;
+	private $twitter_client;
 
 	public function __construct(
-		Oauth1 $oauth1,
-		$base_url
+		GuzzleHttpClient $twitter_client
 	) {
-		$this->oauth1 = $oauth1;
-		$this->base_url = $base_url;
+		$this->twitter_client = $twitter_client;
 	}
 
 	public function getTweets($username)
 	{
-		$stack = HandlerStack::create();
-		$oauth = new Oauth1([
-			'consumer_key'    => 'Ct67CMSMLMygbIO07np38U4KV',
-			'consumer_secret' => 'Ft4CS5tE0VLQ3OiHWSlaABQeADsslVRywBUghdelT4mw2hwPq4',
-			'token'           => '728145508351385600-hkiOhSoBkrDBimUMhfCN6vSu6EzUJth',
-			'token_secret'    => 'F2oxc0DoT9Va02Vq6fevENKZLBzk50tk9dlvaX71xOFnc'
-		]);
-		$stack->push($oauth);
-		$client = new Client([
-			'base_uri' => $this->base_url,
-			'handler' => $stack,
-			'auth' => 'oauth'
-		]);
+		$res = $this->twitter_client->get('statuses/home_timeline.json?screen_name='.$username.'&count=5');
 
-		$res = $client->get('statuses/home_timeline.json?screen_name='.$username.'&count=5')->getBody();
 		return $res;
 	}
 }
